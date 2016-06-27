3de4616a685b474df2392a293d4bfd563174f1c4
adriantodt <adriantodt@outlook.com>
2016-06-26 21:27:25 -0300

    Factories merged

--- src/main/java/cf/brforgers/core/lib/FastFactory.java
+++ src/main/java/cf/brforgers/core/lib/FastFactory.java
@@ -3,12 +3,16 @@ package cf.brforgers.core.lib;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 
-public class BlockFactory {
+public class FastFactory {
 	public static class FactoryBlock extends Block {protected FactoryBlock(Material mat) {super(mat);}}
+	public static class FactoryItem extends Item {protected FactoryItem(){}}
+	
 	public CreativeTabs defaultTab;
 	public String texturePrefix;
 	public Material defaultMaterial;
+
 	public Block newBlock(String name)
 	{
 		return new FactoryBlock(defaultMaterial).setBlockName(name).setBlockTextureName(texturePrefix + name).setCreativeTab(defaultTab);
@@ -19,12 +23,22 @@ public class BlockFactory {
 		return block.setBlockName(name).setBlockTextureName(texturePrefix + name).setCreativeTab(defaultTab);
 	}
 	
-	public static BlockFactory newFactory(CreativeTabs defaultTab, String texturePrefix, Material defaultMaterial) {
-		BlockFactory f = new BlockFactory();
+	public Item newItem(String name)
+	{
+		return new FactoryItem().setUnlocalizedName(name).setTextureName(texturePrefix + name).setCreativeTab(defaultTab);
+	}
+	
+	public Item processItem(Item item, String name)
+	{
+		return item.setUnlocalizedName(name).setTextureName(texturePrefix + name).setCreativeTab(defaultTab);
+	}
+	
+	public static FastFactory newFactory(CreativeTabs defaultTab, String texturePrefix, Material defaultMaterial) {
+		FastFactory f = new FastFactory();
 		f.defaultTab = defaultTab;
 		f.texturePrefix = texturePrefix;
 		f.defaultMaterial = defaultMaterial;
 		return f;
 	}
-	private BlockFactory() {}
+	private FastFactory() {}
 }
